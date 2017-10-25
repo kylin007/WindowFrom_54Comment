246fb3d97f49d17719c6cdba5e1d861e95a38866
yishilin <1052150650@qq.com>
2017-10-25 18:16:08 +0800

    first commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.userBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._54CommentDataSet = new WindowsFormsApplication1._54CommentDataSet();
+            this.userTableAdapter = new WindowsFormsApplication1._54CommentDataSetTableAdapters.UserTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.temporaryTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._54CommentDataSet1 = new WindowsFormsApplication1._54CommentDataSet1();
+            this.temporaryTableTableAdapter = new WindowsFormsApplication1._54CommentDataSet1TableAdapters.TemporaryTableTableAdapter();
+            this._54CommentDataSet2 = new WindowsFormsApplication1._54CommentDataSet2();
+            this.userBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.userTableAdapter1 = new WindowsFormsApplication1._54CommentDataSet2TableAdapters.UserTableAdapter();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.UserId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._54CommentDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.temporaryTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._54CommentDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._54CommentDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1048, 512);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.UserId});
+            this.dataGridView1.Location = new System.Drawing.Point(1066, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(198, 518);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // userBindingSource
+            // 
+            this.userBindingSource.DataMember = "User";
+            this.userBindingSource.DataSource = this._54CommentDataSet;
+            // 
+            // _54CommentDataSet
+            // 
+            this._54CommentDataSet.DataSetName = "_54CommentDataSet";
+            this._54CommentDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // userTableAdapter
+            // 
+            this.userTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(1079, 623);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "随机若干条生成评论";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1186, 623);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "提交数据";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToResizeColumns = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 530);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(1048, 158);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // temporaryTableBindingSource
+            // 
+            this.temporaryTableBindingSource.DataMember = "TemporaryTable";
+            this.temporaryTableBindingSource.DataSource = this._54CommentDataSet1;
+            // 
+            // _54CommentDataSet1
+            // 
+            this._54CommentDataSet1.DataSetName = "_54CommentDataSet1";
+            this._54CommentDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // temporaryTableTableAdapter
+            // 
+            this.temporaryTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // _54CommentDataSet2
+            // 
+            this._54CommentDataSet2.DataSetName = "_54CommentDataSet2";
+            this._54CommentDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // userBindingSource1
+            // 
+            this.userBindingSource1.DataMember = "User";
+            this.userBindingSource1.DataSource = this._54CommentDataSet2;
+            // 
+            // userTableAdapter1
+            // 
+            this.userTableAdapter1.ClearBeforeFill = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1092, 564);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 38);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "将修改同步至数据库";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            // 
+            // UserId
+            // 
+            this.UserId.DataPropertyName = "UserId";
+            this.UserId.HeaderText = "UserId";
+            this.UserId.Name = "UserId";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1295, 700);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._54CommentDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.temporaryTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._54CommentDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._54CommentDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private _54CommentDataSet _54CommentDataSet;
+        private System.Windows.Forms.BindingSource userBindingSource;
+        private _54CommentDataSetTableAdapters.UserTableAdapter userTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private _54CommentDataSet1 _54CommentDataSet1;
+        private System.Windows.Forms.BindingSource temporaryTableBindingSource;
+        private _54CommentDataSet1TableAdapters.TemporaryTableTableAdapter temporaryTableTableAdapter;
+        private _54CommentDataSet2 _54CommentDataSet2;
+        private System.Windows.Forms.BindingSource userBindingSource1;
+        private _54CommentDataSet2TableAdapters.UserTableAdapter userTableAdapter1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserId;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        DataTable datatable_user = new DataTable();
+        DataTable datatable_linshiComm = new DataTable();
+        SqlDataAdapter dapt = new SqlDataAdapter();
+        SqlDataAdapter dapt2 = new SqlDataAdapter();
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=54Comment;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+            //必须进行初始化，否则就出来页面啦。
+            CefSharp.Cef.Initialize();
+
+            //实例化控件
+            ChromiumWebBrowser wb = new ChromiumWebBrowser("http://www.hi-54.com");
+            //设置停靠方式
+            wb.Dock = DockStyle.Fill;
+
+            wb.LifeSpanHandler = new OpenPageSelf();
+            //加入到当前窗体中
+            //this.Controls.Add(wb);
+            groupBox1.Controls.Add(wb);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO:  这行代码将数据加载到表“_54CommentDataSet2.User”中。您可以根据需要移动或删除它。
+            //this.userTableAdapter1.Fill(this._54CommentDataSet2.User);
+            // TODO:  这行代码将数据加载到表“_54CommentDataSet1.TemporaryTable”中。您可以根据需要移动或删除它。
+            //this.temporaryTableTableAdapter.Fill(this._54CommentDataSet1.TemporaryTable);
+            // TODO:  这行代码将数据加载到表“_54CommentDataSet.User”中。您可以根据需要移动或删除它。
+            //this.userTableAdapter.Fill(this._54CommentDataSet.User);
+            //WebView webView = new WebView();
+            //webView.Address = "http://www.baidu.com";
+            //webView.Dock = DockStyle.Fill;
+            //this.Controls.Add(webView);
+            
+            conn.Open();
+            string sql = "select * from [User]";
+            dapt = new SqlDataAdapter(sql, conn);
+            dapt.Fill(datatable_user);
+            dataGridView1.DataSource = datatable_user;
+
+            sql = "select * from [TemporaryTable]";
+            dapt2 = new SqlDataAdapter(sql, conn);
+            dapt2.Fill(datatable_linshiComm);
+            dataGridView2.DataSource = datatable_linshiComm;
+            conn.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //int count = dataGridView1.SelectedRows.Count;
+            int a_Index = dataGridView1.CurrentRow.Index;
+            string str = dataGridView1.Rows[a_Index].Cells[0].Value.ToString();
+            //MessageBox.Show("str");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommandBuilder builder = new SqlCommandBuilder(dapt);
+                dapt.Update(datatable_user);
+                builder = new SqlCommandBuilder(dapt2);
+                dapt2.Update(datatable_linshiComm);
+
+                MessageBox.Show("成功");
+            }
+            catch
+            {
+                MessageBox.Show("格式出现异常，请注意格式");
+            }
+            
+        }
+    }
+}
